07b0d0527d4356684931144c737b20c47222f9bd
Cornnnel <cugut05@gmail.com>
2024-04-12 09:51:01 +0300

    added tasks

---
+++ lesson1.py
@@ -0,0 +1,71 @@
+# age = 18
+#
+# print(age)
+#
+# age = 20
+#
+# print(age)
+#
+# age2 = age
+#
+# print(age2)
+#
+# del(age)
+#
+# print(age)
+
+
+    # task 2
+# is_student = True
+#
+# print(is_student)
+#
+# is_child = False
+#
+# print(is_child)
+#
+# print(is_student and is_child)
+#
+# print(is_student or is_child)
+#
+# print(not is_student)
+
+# task 3
+
+number = 10
+
+print(number)
+
+number2 = number
+
+print(number2)
+
+number3 = 4
+
+print(number + number3)
+print(number - number3)
+print(number * number3)
+print(number / number3)
+print(number % number3)
+print(number ** number3)
+print(number // number3)
+
+number4 = 1_000_000_000_000
+print(number4)
+
+numar=5.0
+
+print(numar)
+
+numar2=2.0
+
+numar_mare=1_000_000
+
+print(numar_mare)
+
+del numar_mare
+
+
+
+
+
